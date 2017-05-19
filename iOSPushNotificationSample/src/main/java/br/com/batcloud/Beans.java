f95ae0b0569ba7ec457b0eaa360cc5984e41db5a
Giovanni Bruno <giovannibruno@Giovannis-MacBook-Pro.local>
2017-05-19 02:57:28 -0300

    swagger added

--- iOSPushNotificationSample/src/main/java/br/com/batcloud/Beans.java
+++ iOSPushNotificationSample/src/main/java/br/com/batcloud/Beans.java
@@ -9,8 +9,14 @@ import com.relayrides.pushy.apns.ApnsClient;
 import com.relayrides.pushy.apns.ApnsClientBuilder;
 
 import io.netty.util.concurrent.Future;
+import springfox.documentation.builders.PathSelectors;
+import springfox.documentation.builders.RequestHandlerSelectors;
+import springfox.documentation.spi.DocumentationType;
+import springfox.documentation.spring.web.plugins.Docket;
+import springfox.documentation.swagger2.annotations.EnableSwagger2;
 
 @Configuration
+@EnableSwagger2
 public class Beans {
 	
 	@Value("${cert.filename}")
@@ -30,4 +36,14 @@ public class Beans {
 		connectFuture.await();
 		return apnsClient;
 	}
+	
+	@Bean
+    public Docket api() { 
+        return new Docket(DocumentationType.SWAGGER_2)  
+        		
+          .select()                                  
+          .apis(RequestHandlerSelectors.any())              
+          .paths(PathSelectors.any())                          
+          .build();                                           
+    }
 }

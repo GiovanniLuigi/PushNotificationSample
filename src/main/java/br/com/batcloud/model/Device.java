4c44d94646def7e8a20d8d4a18b929678df3a385
Giovanni Bruno <giovannibruno@Giovannis-MacBook-Pro.local>
2017-05-20 17:14:07 -0300

    token unique validation

--- src/main/java/br/com/batcloud/model/Device.java
+++ src/main/java/br/com/batcloud/model/Device.java
@@ -2,6 +2,7 @@ package br.com.batcloud.model;
 
 import java.io.Serializable;
 
+import javax.persistence.Column;
 import javax.persistence.Entity;
 import javax.persistence.GeneratedValue;
 import javax.persistence.GenerationType;
@@ -16,6 +17,7 @@ public class Device implements Serializable{
 	@GeneratedValue(strategy = GenerationType.AUTO)
 	private Long id;
 	
+	@Column(unique = true)
 	private String token;
 	
 	public Device() {
